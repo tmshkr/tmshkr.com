15ffabcd0cee67f3e3dffaad2794262fbdc48d9f
Tim Shaker <tim@tmshkr.com>
2019-08-19 17:10:08 -0700

    restore files

---
+++ src/pages/home.js
@@ -0,0 +1,48 @@
+import React, { Fragment } from "react"
+import SEO from "../components/seo"
+import Navbar from "../components/navbar"
+import Projects from "./projects"
+import ArrowDown from "@fortawesome/fontawesome-free/svgs/solid/arrow-down.svg"
+import "./home.scss"
+
+function Home(props) {
+  const scrollTo = id => {
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    })
+  }
+  return (
+    <Fragment>
+      <SEO />
+      <Navbar />
+      <main id="home">
+        <div id="hello" className="full-page" onClick={() => scrollTo("bio")}>
+          <h1 className="hero">Hello, I'm Tim</h1>
+          <p>I design and develop web applications for humans.</p>
+          <ArrowDown className="arrow bounce" />
+        </div>
+        <div
+          id="bio"
+          className="full-page"
+          onClick={() => scrollTo("projects")}
+        >
+          <p>
+            I grew up with computers and have been familiar with web
+            technologies for most of my life. I'm at home with developer tools
+            and am no stranger to the command line.
+          </p>
+          <p>
+            I've studied art, design, science, and most recently earned a degree
+            in philosophy. After graduating, I rekindled my interest in
+            technology, particularly in using React to make web applications.
+          </p>
+        </div>
+        <Projects />
+      </main>
+    </Fragment>
+  )
+}
+
+export default Home
